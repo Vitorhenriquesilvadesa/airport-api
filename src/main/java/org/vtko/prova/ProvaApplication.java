1095121c92f0f0c1340af3f4efa7399bc162e295
Vitorhenriquesilvasdesa <vitorhenriquesilvadesa@gmail.com>
2025-05-12 11:19:32 -0300

    First commit

---
+++ src/main/java/org/vtko/prova/ProvaApplication.java
@@ -0,0 +1,11 @@
+package org.vtko.prova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProvaApplication.class, args);
+    }
+}
